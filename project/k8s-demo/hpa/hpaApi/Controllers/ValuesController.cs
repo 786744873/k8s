9e41d1c731c33f64ab7db7e9cddb849553784bb9
786744873 <786744873@qq.com>
2020-02-22 01:07:28 +0800

    测试阿里云内置镜像规则master

--- project/k8s-demo/hpa/hpaApi/Controllers/ValuesController.cs
+++ project/k8s-demo/hpa/hpaApi/Controllers/ValuesController.cs
@@ -37,7 +37,7 @@ namespace hpaApi.Controllers
             Console.WriteLine("完成非并行计算,耗时(ms)"+watch.ElapsedMilliseconds);
             Console.WriteLine("性能比" + 100000d/watch.ElapsedMilliseconds);
            
-            return Ok("v0.0.6");
+            return Ok("v0.0.2");
         }
 
         // GET api/values/5
